b64f97125c8e6446fa1a2f14e5935c55d52b9483
William E Hahn <williamedwardhahn@gmail.com>
2021-08-26 23:08:43 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+   name='DeepZoo',
+   version='1.0',
+   description='Deep Zoo Artificial Life / Intelligence Toolkit',
+   author='William Edward Hahn',
+   author_email='williamedwardhahn@gmail.com',
+   packages=['DeepZoo'],
+   install_requires=[],
+)
