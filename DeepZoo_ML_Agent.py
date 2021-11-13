75c7ad01d4fec3b3295330eb624ff9dd2c46f92e
William E Hahn <williamedwardhahn@gmail.com>
2021-11-13 16:37:28 -0500

    Update DeepZoo_ML_Agent.py

--- DeepZoo_ML_Agent.py
+++ DeepZoo_ML_Agent.py
@@ -1,28 +1,20 @@
-import os
-os.system("pip install --ignore-installed  git+https://github.com/williamedwardhahn/DeepZoo")
+# import os
+# os.system("pip install --ignore-installed  git+https://github.com/williamedwardhahn/DeepZoo")
+# Action Vector: 
+# Dim0 -> 1 Foward / 2 Reverse, 
+# Dim1 -> 1 Strafe Right / 2 Strafe Left, 
+# Dim2 -> 1 Turn Counter / 2 Turn Clock, 
+# Dim3 -> 0 Null / 1 Null
+#########################################
 from DeepZoo import *
 
-env = start("Fox1")
+env = start("Fox2")
 
 for i in range(10):
 
-    action = np.array([[1,0,0,0]])
+    action = np.array([[1,0,0,0]])      
     
     state,reward = step(env,action)
 
-    plt.imshow(state)
-    plt.show()
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    plot(state)
+    print(reward)
