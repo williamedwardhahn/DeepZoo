4a77c837d792bdcf889a8dbbf4189ba4ab863f04
William E Hahn <williamedwardhahn@gmail.com>
2021-11-13 15:59:35 -0500

    Update __init__.py

--- DeepZoo/__init__.py
+++ DeepZoo/__init__.py
@@ -91,3 +91,37 @@ def clear(screen):
 	
 def load(image):
 	return pygame.surfarray.make_surface(image)
+
+
+
+
+
+
+
+
+
+
+########
+#Unity API
+from mlagents_envs.environment import UnityEnvironment
+from mlagents_envs.environment import ActionTuple, BaseEnv
+import matplotlib.pyplot as plt
+import numpy as np
+
+def step(action):
+    action_tuple = ActionTuple()
+    action_tuple.add_discrete(action)
+    behavior_name = list(env.behavior_specs)[0]
+    env.set_actions(behavior_name, action_tuple)
+    env.step()
+    decision_steps, terminal_steps = env.get_steps(behavior_name)
+    if len(terminal_steps) > 0:
+        s = terminal_steps.obs[0][0,:,:,:]
+        r = terminal_steps.reward 
+        env.reset()
+    else: 
+        s = decision_steps.obs[0][0,:,:,:]
+        r = decision_steps.reward
+    return s,r
+    
+
